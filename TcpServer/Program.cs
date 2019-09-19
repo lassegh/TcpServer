c8c3e3ec6299bd327f269daac0e1a1fdf2c07d2f
lassegh <lasse.groenbech@gmail.com>
2019-09-19 14:42:48 +0200

    Initial commit

---
+++ TcpServer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TcpServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server s = new Server();
+            s.Start();
+        }
+    }
+}
---
+++ TcpServer/Server.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using BookLibrary;
+using Newtonsoft.Json;
+
+namespace TcpServer
+{
+    public class Server
+    {
+        private static List<Book> bookList = new List<Book>
+        {
+            new Book("I offentlighedens tjeneste",400,"9788793772137","Politiken"),
+            new Book("Simones perlebog",100,"9788793825000","Simone"),
+            new Book("Ternet Ninja",272,"9788763859646","Anders Matthesen"),
+            new Book("De smaa synger",127,"9788714195519","Benita Haastrup"),
+            new Book("Funny Face",25,"9978814155520","Funny Face")
+        };
+
+        public void Start()
+        {
+            TcpListener tcpServer = new TcpListener(IPAddress.Loopback, 4646);// Loopback er ens egen maskine
+            tcpServer.Start();
+
+            while (true)
+            {
+                TcpClient socket = tcpServer.AcceptTcpClient(); // Denne venter hårdt på en client
+                
+                    // Starter ny tråd
+                    Task.Run(() =>
+                    {
+                        // Indsætter metoder (delegate) som lambda
+                        TcpClient tmpSocket = socket;
+                        Run(tmpSocket);
+
+                    });
+                
+            }
+        }
+
+        public void Run(TcpClient socket)
+        {
+            using (StreamReader sr = new StreamReader(socket.GetStream()))
+            using (StreamWriter sw = new StreamWriter(socket.GetStream()))
+            {
+                while (true)
+                {
+                    string lineOne = sr.ReadLine();
+                    string lineTwo;
+
+                    switch (lineOne.ToLower())
+                    {
+                        case "getall":
+                            // json list
+                            sw.WriteLine(JsonConvert.SerializeObject(bookList));
+                            sw.Flush();
+                            break;
+                        case "getone":
+                            sw.WriteLine("Enter ISBN:");
+                            sw.Flush();
+                            lineTwo = sr.ReadLine();
+                            // json object
+                            sw.WriteLine(JsonConvert.SerializeObject(bookList.First(b => b.ISbn == lineTwo)));
+                            sw.Flush();
+                            break;
+                        case "save":
+                            sw.WriteLine("Enter book as json:");
+                            sw.Flush();
+                            lineTwo = sr.ReadLine();
+                            // TODO save json object
+                            bookList.Add(JsonConvert.DeserializeObject<Book>(lineTwo));
+                            sw.Flush();
+                            break;
+                        default:
+                            sw.WriteLine("Use 'GetAll', 'Get' or 'Save'");
+                            sw.Flush();
+                            break;
+
+                    }
+                }
+            }
+            /// Bogobjekter i json skal skrives i dette format:
+            /// {"Title":"I offentlighedens tjeneste","Author":"Politiken","NumberOfPages":400,"ISbn":"9788793772137"}
+
+
+            socket?.Close();
+        }
+    }
+}
